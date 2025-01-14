6049c46aaac3d746304cac474aa498caf39c2e6a
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2025-01-14 07:26:17 -0500

    fix: bug in authentication with username and password

--- src/main/java/com/security/security/example/config/SecurityConfig.java
+++ src/main/java/com/security/security/example/config/SecurityConfig.java
@@ -2,29 +2,58 @@ package com.security.security.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
-@EnableMethodSecurity
+@EnableMethodSecurity //PERMITE CONFIGURAR CON ANOTACIONES
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-         return httpSecurity
-                 //.csrf(csrf -> csrf.disable()) //Evitar captacion de datos de formularios, si no se trabajara con formularios, desactivar
-                 .authorizeHttpRequests(authorize -> authorize //manejar loe endpoints para indicar cuales son publicos y cuales no
-                     .requestMatchers("/v1/index2").permitAll() //rutas libres
-                     .anyRequest().authenticated()) //rutas que deben estar autenticadas SIN ROL ESPECIFICO
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+//         return httpSecurity
+//                 .csrf(csrf -> csrf.disable()) //Evitar captacion de datos de formularios solo thymeleaf, si no se trabajara con formularios, desactivar
+//                 .httpBasic(Customizer.withDefaults()) //PERMITE AUTENTICACION BASICA, ES DECIR USUARIO Y CONTRASEÑA
+//                 //STATELESS, quiere decir que no se trabajara con sesion, es decir, no se creara una sesion en la base de datos como en laravel, si no que se trabajara con tokwn
+//                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                 //PERMISOS A ENDPOINTS: METODO Y RUTA
+//                 .authorizeHttpRequests(httpRequests -> {
+//                     httpRequests.requestMatchers(HttpMethod.GET, "/auth/index2").permitAll(); //publico
+//                     httpRequests.requestMatchers(HttpMethod.GET, "/auth/index").hasAuthority("READ"); //debe tener permiso de leer PRIVADO
+//
+//                     httpRequests.anyRequest().denyAll(); //cualquier otro trquest, que no este definido aca, deniega el acceso
+//
+////                     httpRequests.anyRequest().authenticated(); Los endpoints no definidos arriba, va a permitir acceso si y solo si el usuario esta autenticado
+//                         })
+//                 .build();
+//    }
 
-                 .build();
-    }
+//FILTRO DE SEGURIDAD POR MEDIO DE ANOTACIONES, RECORDAR QUE LOS CONTROLADORES DEBEN TENER @PreAuthorize("denyAll()")
+
+@Bean
+public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity
+            .csrf(csrf -> csrf.disable()) //Evitar captacion de datos de formularios solo thymeleaf, si no se trabajara con formularios, desactivar
+            .httpBasic(Customizer.withDefaults()) //PERMITE AUTENTICACION BASICA, ES DECIR USUARIO Y CONTRASEÑA
+            //STATELESS, quiere decir que no se trabajara con sesion, es decir, no se creara una sesion en la base de datos como en laravel, si no que se trabajara con tokwn
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .build();
+}
 
     @Bean
     public AuthenticationManager authenticationManaer(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -34,8 +63,25 @@ public class SecurityConfig {
     @Bean //Creamos provedor de auntenticacion junto con sus 2 componentes
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setPasswordEncoder(null); //hashea la contraseña y la compara con la de la database
-        authenticationProvider.setUserDetailsService(null); //tRAE EL USUARIO DE LA DATABASE
+        authenticationProvider.setPasswordEncoder(passwordEncoder()); //hashea la contraseña y la compara con la de la database
+        authenticationProvider.setUserDetailsService(userDetailsService()); //tRAE EL USUARIO DE LA DATABASE
         return authenticationProvider;
     }
+
+    //DEBE ESTAR CONECTADO A UNA DB, Al devolver el usuario de la data base debe castearse a tipo UserDetails
+    @Bean
+    public UserDetailsService userDetailsService() {
+        //PODEMOS TAMBIEN RETORNAR VARIOS UUSARIOS CON List<UserDetails>
+        UserDetails userDetails = User.withUsername("juan") //User es el user de spring security
+                .password("1234")
+                .roles("ADMIN")
+                .authorities("READ","CREATE")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance(); //SOLO EJEMPLO, NO HASHEA CONTRASEÑAS!!!
+    }
+
 }
--- src/main/java/com/security/security/example/controllers/TestAuthController.java
+++ src/main/java/com/security/security/example/controllers/TestAuthController.java
@@ -1,11 +1,13 @@
 package com.security.security.example.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/auth")
 @RestController
+@PreAuthorize("denyAll()")
 public class TestAuthController {
 
     @GetMapping("/index")
